d8347276e77eafa74a95150bad4a2b8e2f673b3b
Ingo Schommer <ingo@silverstripe.com>
2015-09-14 22:42:56 +1200

    Initial commit

---
+++ client/index.jsx
@@ -0,0 +1,102 @@
+App = React.createClass({
+  mixins: [ReactMeteorData],
+
+  getMeteorData() {
+    var subHandle = Meteor.subscribe('videos');
+    
+    return {
+      videos: Videos.find({}).fetch(),
+      videosLoading: ! subHandle.ready(),
+      isSubmitting: Session.get('isSubmitting')
+    }
+  },
+
+  handleSubmit(event) {
+    var url;
+
+    event.preventDefault()
+    Session.set('isSubmitting', true)
+    
+    url = React.findDOMNode(this.refs.textInput).value.trim()
+    Meteor.call('videos/add', url, (err, res) => {
+      Session.set('isSubmitting', false)
+      if(err) {
+        alert(err.error)
+        return
+      }
+    })
+
+    // Clear form
+    React.findDOMNode(this.refs.textInput).value = ""
+  },
+
+  renderVideos() {
+    return this.data.videos.map((video) => {
+      return <Video key={video._id} video={video} />
+    })
+  },
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <header className="page-header">
+            <h1>Videos</h1>
+          </header>
+        </div>
+        <div className="row">
+          <form className="new-video form-inline" onSubmit={this.handleSubmit} >
+            <input
+              type="text"
+              className="form-control"
+              ref="textInput"
+              required
+              placeholder="Add a video URL" />
+            <button 
+              type="submit" 
+              className="btn btn-primary"
+              disabled={this.data.isSubmitting ? 'disabled' : ''}
+            >
+              Add
+            </button>
+          </form>
+        </div>
+        <div className="row panel panel-default">
+          <div className="panel-body">
+            <ul className="media-list">
+              {this.renderVideos()}
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+})
+
+Video = React.createClass({
+  propTypes: {
+    // This component gets the video to display through a React prop.
+    // We can use propTypes to indicate it is required
+    video: React.PropTypes.object.isRequired
+  },
+  render() {
+    return (
+      <li className="media">
+        <div className="media-left">
+          <a href="">
+            <img src={this.props.video.thumbnailUrl} alt={this.props.video.title} className="media-object" />
+          </a>
+        </div>
+        <div className="media-body">
+          <h4 className="media-heading"><a href={this.props.video.url}>{this.props.video.title}</a></h4>
+          <p>{this.props.video.description}</p>
+        </div>
+      </li>
+    )
+  }
+})
+
+Meteor.startup(function () {
+  // Use Meteor.startup to render the component after the page is ready
+  React.render(<App />, document.getElementById("app"))
+})
---
+++ lib/videos.jsx
@@ -0,0 +1,46 @@
+Videos = new Mongo.Collection('videos');
+
+Meteor.methods({
+  'videos/add': (url) => {
+    var response, embedData, video, service, existing;
+
+    // oEmbed needs to run on server
+    if(!Meteor.isServer) {
+      return
+    }
+
+    service = Meteor.npmRequire('oembed-node').init();
+    response = Async.runSync(function(done) {
+      service.get({url: url}, function(err, data) {
+        done(err, data)
+      })
+    })
+    
+    if(response.error) {
+      throw new Meteor.Error('No video found')
+    }
+    
+    embedData = response.result;
+
+    // Check for dupllicates
+    console.log(embedData)
+    if(Videos.findOne({url: embedData.video_url})) {
+      throw new Meteor.Error('Video already exists')
+    }
+
+    // Insert video
+    video = {
+      url: embedData.video_url,
+      thumbnailUrl: embedData.thumbnail_url,
+      thumbnailWidth: embedData.thumbnail_width,
+      thumbnailHeight: embedData.thumbnail_height,
+      title: embedData.title,
+      html: embedData.html,
+      duration: embedData.duration,
+      description: embedData.description
+    }
+    id = Videos.insert(video)
+
+    return id;
+  }
+})
